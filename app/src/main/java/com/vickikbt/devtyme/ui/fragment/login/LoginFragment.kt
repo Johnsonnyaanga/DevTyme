3d97ae74f09ace4d7e3af6683673038c43770132
VictorKabata <39780120+VictorKabata@users.noreply.github.com>
2021-05-27 01:26:28 +0300

    Added navigation to browser tab

--- app/src/main/java/com/vickikbt/devtyme/ui/fragment/login/LoginFragment.kt
+++ app/src/main/java/com/vickikbt/devtyme/ui/fragment/login/LoginFragment.kt
@@ -1,5 +1,7 @@
 package com.vickikbt.devtyme.ui.fragment.login
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,15 +11,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.vickikbt.devtyme.R
 import com.vickikbt.devtyme.databinding.FragmentLoginBinding
+import com.vickikbt.devtyme.utils.Constants
 import com.vickikbt.devtyme.utils.StateListener
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class LoginFragment : Fragment(), StateListener {
 
     private lateinit var binding: FragmentLoginBinding
     private val viewModel by viewModels<LoginViewModel>()
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,18 +37,45 @@ class LoginFragment : Fragment(), StateListener {
     }
 
     private fun initUI() {
+        binding.buttonSignIn.setOnClickListener {
+            openBrowserWindow()
+        }
+    }
 
+    private fun openBrowserWindow() {
+        val intent = Intent.ACTION_VIEW
+        val url = Uri.parse(Constants.WEB_URL)
+        val openBrowserIntent = Intent(intent, url)
+        requireActivity().startActivity(openBrowserIntent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val callbackUri = requireActivity().intent.data
+
+        if (callbackUri != null && callbackUri.toString().startsWith(Constants.REDIRECT_URL)) {
+            val code = callbackUri.getQueryParameter("code")
+            if (code != null) {
+                Timber.e("Code fetched: $code")
+                viewModel.getNewAccessToken(code)
+            } else callbackUri.getQueryParameter("error")?.let {
+                Timber.e(RuntimeException(it))
+            }
+        } else {
+            Timber.e("Nothing was returned!")
+        }
     }
 
     override fun onLoading() {
-
+        Timber.e("Loading...")
     }
 
     override fun onSuccess(message: String?) {
-
+        Timber.e(message)
     }
 
     override fun onError(exception: Exception, message: String?) {
-
+        Timber.e(message)
     }
 }
