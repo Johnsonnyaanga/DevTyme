96dcd1414cf5cd0583d95b28d83af9c9f126c8c7
Kagiri11 <charlesmaish423@gmail.com>
2021-06-01 12:45:02 +0300

    Serialized leader data

--- app/src/main/java/com/vickikbt/devtyme/models/Leaders.kt
+++ app/src/main/java/com/vickikbt/devtyme/models/Leaders.kt
@@ -1,13 +1,24 @@
 package com.vickikbt.devtyme.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Leaders(
-    val current_user: Any,
-    val `data`: List<Data>,
-    val language: Any,
-    val modified_at: String,
-    val page: Int,
-    val range: Range,
-    val timeout: Int,
-    val total_pages: Int,
-    val writes_only: Boolean
+    @SerializedName("current_user")
+    val currentUser: Any?=null,
+    @SerializedName("data")
+    val board: List<Data>?=null,
+    @SerializedName("language")
+    val language: Language,
+    @SerializedName("modified_at")
+    val modifiedAt: String?=null,
+    @SerializedName("page")
+    val page: Int?=null,
+    @SerializedName("range")
+    val range: Range?=null,
+    @SerializedName("timeout")
+    val timeout: Int?=null,
+    @SerializedName("total_pages")
+    val totalPages: Int?=null,
+    @SerializedName("writes_only")
+    val writesOnly: Boolean?=null,
 )
